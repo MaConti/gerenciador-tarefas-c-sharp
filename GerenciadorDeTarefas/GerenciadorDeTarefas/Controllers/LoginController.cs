fd9d42a82b74746392890a54240fa20576f6cfd0
MaConti <matheusconti@hotmail.com>
2021-05-31 22:04:38 -0300

    Aula 2 - C#

--- GerenciadorDeTarefas/GerenciadorDeTarefas/Controllers/LoginController.cs
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using GerenciadorDeTarefas.Dtos;
+using GerenciadorDeTarefas.Models;
+using GerenciadorDeTarefas.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -15,6 +17,11 @@ namespace GerenciadorDeTarefas.Controllers
     {
         private readonly ILogger<LoginController> _logger;
 
+        private readonly string loginTeste = "admin@admin.com";
+        private readonly string senhaTeste = "Admin1234@";
+
+
+
         public LoginController(ILogger<LoginController> logger)
         {
             _logger = logger;
@@ -25,7 +32,11 @@ namespace GerenciadorDeTarefas.Controllers
         {
             try
             {
-                if (requisicao == null || requisicao.Login == null || requisicao.Senha == null)
+                if (requisicao == null || requisicao.Login == null || requisicao.Senha == null
+                    || string.IsNullOrEmpty(requisicao.Login) || string.IsNullOrWhiteSpace(requisicao.Login)
+                    || string.IsNullOrEmpty(requisicao.Senha) || string.IsNullOrWhiteSpace(requisicao.Senha)
+                    || requisicao.Login != loginTeste || requisicao.Senha != senhaTeste);
+
                 {
                     return BadRequest(new ErroRespostaDto()
                     {
@@ -34,8 +45,22 @@ namespace GerenciadorDeTarefas.Controllers
                     });
                 }
 
-                return Ok("Usuário autenticado com sucesso");
-            }
+                var usuarioTeste = new Usuario()
+                {
+                    Id = 1,
+                    Nome = "Usuario Teste",
+                    Email = loginTeste,
+                    Senha = senhaTeste
+                };
+
+                var token = TokenService.CriarToken(usuarioTeste);
+
+                return Ok(new LoginRespostaDto()
+                {
+                    Status = StatusCodes.Status400BadRequest,
+                    Erro = "Parâmetros de entrada inválidos"
+                }
+
             catch (Exception excecao)
             {
                 _logger.LogError($"Ocorreu erro ao efetuar login: {excecao.Message}", excecao);
--- GerenciadorDeTarefas/GerenciadorDeTarefas/Startup.cs
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace GerenciadorDeTarefas
@@ -32,6 +33,8 @@ namespace GerenciadorDeTarefas
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GerenciadorDeTarefas", Version = "v1" });
             });
+
+     
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
